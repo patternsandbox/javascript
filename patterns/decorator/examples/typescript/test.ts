d5418c230981ed89c2c0997f91f6ca186986c50e
Girma Nigusse <xgirma@gmail.com>
2021-06-12 21:06:33 -0400

    add test

--- patterns/decorator/examples/typescript/test.ts
+++ patterns/decorator/examples/typescript/test.ts
@@ -1,1 +1,11 @@
-// TODO
+import Car from "./index";
+
+test("Stage 2 draft function decorator", () => {
+    const upgraded = new Car({ body: "sedan", model: "Camry", sunroof: false });
+
+    const result = upgraded.assemble();
+
+    expect(result.tint).toBeTruthy();
+    expect(result.color).toEqual("Yellow");
+    expect(result.model).toEqual("Camry");
+});
